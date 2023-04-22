683cf9d9865d7d5959c5e099bc1f7563c7d0c6e7
adilakshmi <adilakshmi@gmail.com>
2023-04-22 22:20:42 +0530

    second-commit

--- upskilling-assignment-3/src/RegisterForm/index.jsx
+++ upskilling-assignment-3/src/RegisterForm/index.jsx
@@ -4,7 +4,6 @@ import { useState } from "react"
 
 /* eslint-disable import/no-anonymous-default-export */
 export default function({sendData}){
-    const [page, setPage]= useState(-1);
     const [UserNames, setUserNames] = useState([]);
     const [passwords, setPasswords] = useState([]);
     return <div style={{margin:"30px 40vw",}}>
@@ -12,6 +11,6 @@ export default function({sendData}){
         <div className="user-input"><span>Enter Email: </span> <input type="text"/></div><br/>
         <div className="user-input" onChange={(event) => {setUserNames(...UserNames, event.target.value)}}><span>Enter UserName: </span> <input type="text"/></div><br/>
         <div className="user-input" onChange={(event) => {setPasswords( ...passwords, event.target.value)}}><span>Enter Password: </span> <input type="password"/></div><br/>
-        <button style={{margin:"0px 90px"}} onClick={() => {setPage(0); sendData(UserNames,passwords,page); } }>Register</button><br/>
+        <button style={{margin:"0px 90px"}} onClick={() => {sendData(UserNames,passwords); } }>Register</button><br/>
     </div>
 }
